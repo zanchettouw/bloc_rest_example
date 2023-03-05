bb146cee2474b72f7533e08ca467b3d9080c56bf
fz.dev <fernandozanca94@gmail.com>
2023-03-05 20:46:45 -0300

    create user bloc

---
+++ lib/blocs/app_blocs.dart
@@ -0,0 +1,20 @@
+import 'package:bloc_rest_example/blocs/app_events.dart';
+import 'package:bloc_rest_example/blocs/app_states.dart';
+import 'package:bloc_rest_example/repo/repositories.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class UserBloc extends Bloc<UserEvent, UserState> {
+  final UserRepository _userRepository;
+
+  UserBloc(this._userRepository) : super(UserLoadingState()) {
+    on<LoadUserEvent>((event, emit) async {
+      emit(UserLoadingState());
+      try {
+        final users = await _userRepository.getUsers();
+        emit(UserLoadedState(users));
+      } catch (e) {
+        emit(UserErrorState(e.toString()));
+      }
+    });
+  }
+}
